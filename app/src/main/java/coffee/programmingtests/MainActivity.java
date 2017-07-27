5642b8ac0f78b3a6c759b2b452118729cfb3d29c
tr1b3 <tribe91@gmx.net>
2017-07-27 16:52:20 +0200

    ArrayList erstellt

--- app/src/main/java/coffee/programmingtests/MainActivity.java
+++ app/src/main/java/coffee/programmingtests/MainActivity.java
@@ -2,6 +2,7 @@ package coffee.programmingtests;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList testArrayList = new ArrayList();
+
+        listEntry testEntry = new listEntry();
+
+        testArrayList.add(testEntry);
     }
+
+
 }
---
+++ app/src/main/java/coffee/programmingtests/listEntry.java
@@ -0,0 +1,16 @@
+package coffee.programmingtests;
+
+/**
+ * Created by hs23c9 on 27.07.17.
+ */
+
+public class listEntry {
+    private int testInteger;
+
+    public listEntry (){}
+
+    public void ficken()
+    {
+
+    }
+}
